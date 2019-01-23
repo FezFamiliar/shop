13c83a1066958d062761510374fe2977fa14d267
Aaron Pop <FezFamiliar@yahoo.com>
2019-01-23 17:17:00 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,54 @@
+<?php
+
+include 'config.php';
+include 'header.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+  $username = mysqli_real_escape_string($conn,$_POST['username']);
+  $password = mysqli_real_escape_string($conn,$_POST['password']);
+
+
+  $query = "SELECT * FROM `users` WHERE username = '".$username."' AND password = '".md5($password)."'";
+  $result = mysqli_query($conn,$query);
+  if(mysqli_num_rows($result) > 0){
+
+    $_SESSION['username'] = $username;
+    header("refresh:0;url=login");
+
+  }else{
+
+    echo 'Username or password doesn\'t match!';
+
+  }
+
+}
+ ?>
+ <div class="wrapper">
+   <form action="login" method="POST" autocomplete="off" id="login-form">
+     <table>
+       <tr>
+         <td><label for="username">Username: </label></td>
+         <td><input type="text" name="username" autocomplete="off" id="username"></td>
+
+       </tr>
+       <tr id="validate-user">
+         <td></td>
+         <td></td>
+       </tr>
+       <tr>
+         <td><label for="password"> Password:</label></td>
+         <td><input type="password" name="password" id="password"></td>
+       </tr>
+       <tr id="validate-pass">
+         <td></td>
+         <td></td>
+       </tr>
+       <tr>
+         <td id="par"></td>
+         <td><input type="submit" value="Login"></td>
+       </tr>
+     </table>
+   </form>
+ </div>
---
+++ parser.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'config.php';
+echo 'f';
+echo 'ff'
+//
+// $path = 'images/meat/';
+// $image_array = [$path.'tomahawk.jpg',$path.'rib.jpg',$path.'sirloin.jpg',$path.'mignion.jpg',
+// $path.'venison.jpg',];
+//
+//
+// $price = 19.99;
+// foreach ($image_array as $images) {
+//
+//   $name = str_replace('images/meat/','',$images);
+//   $name = str_replace('.jpg','',$name);
+//   mysqli_query($conn,"INSERT INTO products(category_id,name,description,image,price) VALUES('5','$name','lorem ipsm ipsum lorem','$images','$price')");
+//   $price +=5;
+// }
+
+
+ ?>
