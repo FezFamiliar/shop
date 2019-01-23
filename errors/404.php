f873a45b15c11bb6ca2110d82fb0eca228b8b1a3
Aaron Pop <FezFamiliar@yahoo.com>
2019-01-23 17:22:36 +0200

    Update 404.php

--- errors/404.php
+++ errors/404.php
@@ -1,1 +1,17 @@
 
+<?
+
+  include '../config.php';
+  include '../header.php';
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1 class="d" >Sorry , no page here!</h1>
+    <a href="index.php" class="a">Take me back</a>
+  </body>
+</html>
